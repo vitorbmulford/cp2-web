80ac1c13184c46dde9cd25787ae8768ed01be63f
Vitor Bebiano Mulford <vitorbmulford@gmail.com>
2024-05-02 14:53:04 -0300

    Criando botões, acesso ao login, criando alart(comentado para nao aparecer toda hora)

--- script.js
+++ script.js
@@ -18,3 +18,27 @@ function slideShow() {
 }
 
 slideShow();
+
+let mudarCor = document.getElementsByClassName("mudar_cor")[0];
+
+let cores = [
+  "rgb(91, 192, 222)", // Verde
+  "rgb(65, 105, 225)", // Azul
+  "rgb(43, 43, 43)", // Original
+];
+
+let indiceCor = 0;
+
+mudarCor.addEventListener("click", () => {
+  document.body.style.backgroundColor = cores[indiceCor];
+
+  indiceCor++;
+
+  if (indiceCor >= cores.length) {
+    indiceCor = 0;
+  }
+});
+
+// window.addEventListener("load", function () {
+//   alert("Bem-vindo à nossa página!");
+// });
